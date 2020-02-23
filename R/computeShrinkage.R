8088e27394a9ce55e745ff72e9c3e346834daea6
Jingshu Wang <wangjingshususan@gmail.com>
2020-02-23 08:27:19 -0600

    save disk usage

--- R/computeShrinkage.R
+++ R/computeShrinkage.R
@@ -51,7 +51,9 @@ computeShrinkage <- function(out.dir, ncores = 1,
 	if (ncores > 1 && ngenes > gene.block.size && ncells > cell.threshold && auto.split) {
 		used.time <- system.time({	
 			system(paste0('mv ', out.dir, '/prediction.rds ', out.dir, '/denoised_est_before_shrinkage.rds'))
-		#	saveRDS(est.mu, paste0(temp.name, "_est_before_shrinkage.rds"))
+			system(paste0('rm ', out.dir, '/prediction.rds'))
+			System.sleep(60)
+			#	saveRDS(est.mu, paste0(temp.name, "_est_before_shrinkage.rds"))
 			sf <- Matrix::colSums(data$mat)
 			sf <- sf / mean(sf)
 			rd <- ceiling(ngenes / gene.block.size)
